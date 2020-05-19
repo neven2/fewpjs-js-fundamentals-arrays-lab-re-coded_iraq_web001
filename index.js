651c4a2a7628fbde19e09f00ca44a97eecba7861
Neven Bedr <neven2225@gmail.com>
2020-05-19 19:17:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 // Write your solution here
 const append=["Milo", "Otis", "Garfield"];
+<<<<<<< HEAD
 const prepend=["Milo", "Otis", "Garfield"];
 const removeLast=["Milo", "Otis", "Garfield"];
 const removeFirst=["Milo", "Otis", "Garfield"];
 append.push('Odie');
 prepend.unshift('Odie');
+=======
+const preppend=["Milo", "Otis", "Garfield"];
+const removeLast=["Milo", "Otis", "Garfield"];
+const removeFirst=["Milo", "Otis", "Garfield"];
+append.push('Odie');
+prappend.unshift('Odie');
+>>>>>>> ccedad220d7f609a6ea7fb75ecd231997b9a452b
 removeLast.pop();
 removeFirst.shift();
